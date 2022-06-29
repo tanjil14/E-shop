87cc8a91592c42f899ed4409105646c9eafc6ddb
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-06-29 23:30:40 +0600

    create private route

--- client/README.md
+++ client/README.md
@@ -3,3 +3,5 @@
 - Create routes and use auth route
 - Create admin login page
 - Implement Redux provider and redux toolkit for auth and store
+- implement reducer for store token
+- implement private route
---
+++ client/src/routes/Private.jsx
@@ -0,0 +1,9 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+const Private = ({ children }) => {
+  const { adminToken } = useSelector((state) => state.authReducer);
+  return adminToken ? children : <Navigate to="/auth/admin-login" />;
+};
+
+export default Private;
