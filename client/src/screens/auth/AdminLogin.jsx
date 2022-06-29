0739c591e6775a45f2721c113ab3ceff7d039068
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-06-29 17:45:49 +0600

    login page error handle

--- client/src/screens/auth/AdminLogin.jsx
+++ client/src/screens/auth/AdminLogin.jsx
@@ -9,20 +9,31 @@ const AdminLogin = () => {
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-  const [login] = useAuthLoginMutation();
+  const [login, response] = useAuthLoginMutation();
+  // console.log("My response", response);
+  const errors = response?.error?.data?.errors
+    ? response?.error?.data?.errors
+    : [];
   const handleSubmit = (e) => {
     e.preventDefault();
     login(data);
+    console.log("click")
   };
   return (
     <div className="bg-black1 h-screen flex justify-center items-center">
       <form
-        onClick={handleSubmit}
+        onSubmit={handleSubmit}
         className="bg-black2 p-6 w-10/12 sm:w-8/12 md:w-6/12 lg:w-3/12"
       >
         <h3 className="text-white capitalize mb-3 font-semibold text-lg">
           dashboard login
         </h3>
+        {errors.length > 0 &&
+          errors.map((error, key) => (
+            <div key={key}>
+              <p className="bg-red-100 text-red-700 p-3 mb-2 rounded-sm text-sm font-medium">{error.msg}</p>
+            </div>
+          ))}
         <div className="mb-3 mt-3">
           <input
             type="email"
@@ -46,7 +57,7 @@ const AdminLogin = () => {
         <div className="mb-3">
           <input
             type="submit"
-            value="Login &rarr;"
+            value={response.isLoading ? "Loading..." : "Login "}
             className="w-full bg-indigo-600 p-3 rounded text-white uppercase font-semibold cursor-pointer"
           />
         </div>
