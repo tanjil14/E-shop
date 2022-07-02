3948d5501ca8a36da1961f40e99f549fa67cbf4c
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-02 23:52:41 +0600

    create product

---
+++ client/src/screens/dashboard/CreateProduct.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "./Wrapper";
+
+const CreateProduct = () => {
+    return (
+        <Wrapper>
+            This is product
+        </Wrapper>
+    );
+};
+
+export default CreateProduct;
